3238634d464cf42ddb36340115b3604d012562d3
DancingCola <stephenm997@yahoo.com>
2014-07-24 22:21:41 -0400

    Preloader added

--- src/images.js
+++ src/images.js
@@ -1,29 +1,47 @@
-function ImageKey(image, name) {
-	this.image = image;
+function ImageKey(path, name) {
 	this.name = name;
+	this.path = path;
 	
 	return this;
 };
 
+var loadedimages = 0;
+
 var image = {
 	list : [],
+	
 	get : function(name) {
 		for (i = 0; i < this.list.length; i ++)
 		{
 			if (this.list[i].name == name)
+			{
 				return this.list[i].image;
+			}
 		}
-		//console.log("Unable to locate image " + name);
 		return 0;
 	},
 	
-	load : function(filename, name) {
+	add : function(filename, name) {
 		if (!this.get(name))
 		{
-			var image = new Image();
-			image.src = filename;
-			this.list.push(new ImageKey(image, name));
+			this.list.push(new ImageKey(filename, name));
 		}
-		//console.log("Image " + name + " already exists");
-	}
-};	
+	},
+	
+	load : function() {
+		for (i = 0; i < this.list.length; i ++)
+			{
+				this.list[i].image = new Image();
+				this.list[i].image.src = this.list[i].path;
+				this.list[i].image.onload = function() {
+					loadedimages ++;
+					if(loadedimages==image.list.length)
+					{
+						initialize();
+					}
+				};
+			}
+		},
+
+}
+
